39340f9a84d91ecc64fa672ea83fdad9302368c4
alexdoffgit <alexdoffacc@gmail.com>
2022-07-18 08:27:05 +0700

    create route for product

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('product', [ProductController::class, 'createProduct']);   //for creating product
+
+Route::put('product/{id}', [ProductController::class, 'updateProduct']); //for updating product
+
+Route::delete('product/{id}', [ProductController::class, 'deleteProduct']);  // for deleting product
+
+Route::get('product', [ProductController::class, 'index']); // for retrieving product
